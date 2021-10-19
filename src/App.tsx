53b1be8e6270dc0999125ee013ff2031cedd6aeb
juanjof1 <salazargiljuanjose@gmail.com>
2021-10-18 21:28:36 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { PLANTS_DATA, Plants } from "./components/data";
+import { Card } from "./components/card";
+
+function App() {
+  const [plants, setPlants] = useState(PLANTS_DATA);
+
+  /** MANEJO DE ESTADO Y ACTUALIZACION DE CANTIDAD DESDE COMPONENTE PADRE */
+  // const [currentId, setCurrentId] = useState(0);
+
+  // const addPlant = () => {
+  //     setPlants((previous:any) => {
+  //       return previous.map((plant:any) => {
+  //         if(plant.id === currentId){
+  //           return {...plant, quantity:plant.quantity+1}
+  //         }
+  //         return plant;
+  //       });
+  //     });
+  //   }
+
+  // const removePlant = () => {
+  //   setPlants((previous:any) => {
+  //     return previous.map((plant:any) => {
+  //       if(plant.id === currentId){
+  //         return {...plant, quantity:plant.quantity-1}
+  //       }
+  //       return plant;
+  //     });
+  //   });
+  // }
+
+  // const setPlant = (id:number) => {
+  //   setCurrentId(id);
+  // }
+
+  return (
+    <div>
+      {plants.length > 0
+        ? plants.map((plant) => (
+            //   <div  key={plant.id} onClick={() => setPlant(plant.id!) }>
+            <div key={plant.id}>
+              <Card
+                title={plant.title}
+                description={plant.description}
+                imgURL={plant.imgURL}
+                quantity={plant.quantity}
+              />
+            </div>
+          ))
+        : null}
+    </div>
+  );
+}
+
+export default App;
